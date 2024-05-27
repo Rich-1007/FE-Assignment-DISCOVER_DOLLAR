eedbb94c0b21b9db9390e0bf7d33152499146347
Rich-1007 <ghricheak@gmail.com>
2024-05-27 10:47:04 +0530

    New UI Updates

--- data/index.js
+++ data/index.js
@@ -1,15 +1,14 @@
-import pices1 from "/assets/Hero-IMG/download-1.webp"
-import pices2 from "/assets/Hero-IMG/download-2.webp"
-import pices3 from "/assets/Hero-IMG/download-3.webp"
-import pices4 from "/assets/Hero-IMG/download-4.webp"
-import pices5 from "/assets/Hero-IMG/download-5.webp"
-
-
+import pices1 from "/assets/Hero-IMG/download-1.webp";
+import pices2 from "/assets/Hero-IMG/download-2.webp";
+import pices3 from "/assets/Hero-IMG/download-3.webp";
+import pices4 from "/assets/Hero-IMG/download-4.webp";
+import pices5 from "/assets/Hero-IMG/download-5.webp";
 
 export const Herodata = [
-  { img:pices5  , title: "Best Truewireless", price: "Grab Now" },
+  { img: pices5, 
+    title: "Best Truewireless", price: "Grab Now" },
   { img: pices4, title: "Noise Smartwatches", price: "From ₹1,099 " },
-  
+
   {
     img: pices3,
     title: "Top Selling Dell",
@@ -21,7 +20,6 @@ export const Herodata = [
     price: "From ₹499",
   },
   {
-  
     img: pices1,
     title: "Best Selling Mobile",
     price: "From ₹499",
--- src/Components/Categoryitem.jsx
+++ src/Components/Categoryitem.jsx
@@ -5,15 +5,15 @@ import {
   IoChevronUpSharp,
 } from "react-icons/io5";
 
-import download1 from "../assets/Icons/download-1.jpg";
-import download2 from "../assets/Icons/download-2.jpg";
-import download3 from "../assets/Icons/download-3.jpg";
-import download4 from "../assets/Icons/download-4.jpg";
-import download5 from "../assets/Icons/download-5.jpg";
-import download6 from "../assets/Icons/download-6.jpg";
-import download7 from "../assets/Icons/download-7.jpg";
-import download8 from "../assets/Icons/download-8.jpg";
-import download9 from "../assets/Icons/download-9.jpg";
+import download1 from "../../public/assets/Icons/download-1.jpg";
+import download2 from "../../public/assets/Icons/download-2.jpg";
+import download3 from "../../public/assets/Icons/download-3.jpg";
+import download4 from "../../public/assets/Icons/download-4.jpg";
+import download5 from "../../public/assets/Icons/download-5.jpg";
+import download6 from "../../public/assets/Icons/download-6.jpg";
+import download7 from "../../public/assets/Icons/download-7.jpg";
+import download8 from "../../public/assets/Icons/download-8.jpg";
+import download9 from "../../public/assets/Icons/download-9.jpg";
 
 // import download from "../assets/Icons/download.jpg"
 
